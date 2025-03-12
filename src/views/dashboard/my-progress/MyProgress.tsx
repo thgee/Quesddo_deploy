fa7a170298bbbf1926c2d6c1eb8d5c2fa53117f0
thgee <thgeee@naver.com>
2025-03-12 00:43:31 +0000

    style(TODO-248): 대시보드 진행상황 스타일링 1차수정 (#187)
    * style: 대시보드 진행상황 스타일링 1차수정
    
    * refactor: 불필요한 변수 제거

--- src/views/dashboard/my-progress/MyProgress.tsx
+++ src/views/dashboard/my-progress/MyProgress.tsx
@@ -8,7 +8,7 @@ export default function MyProgress() {
     <section className="flex h-[250px] flex-1 rounded-xl bg-blue-500 text-white transition-shadow duration-300 hover:shadow-2xl">
       <div className="flex-1 pt-[16px] pl-[24px]">
         <Image src={progressImg} width={40} height={40} alt="progress" />
-        <p className="mt-[16px] font-normal">내 진행 상황</p>
+        <p className="mt-[16px] text-2xl font-semibold">내 진행 상황</p>
       </div>
       <Gauge />
     </section>
--- src/views/dashboard/my-progress/components/Gauge.tsx
+++ src/views/dashboard/my-progress/components/Gauge.tsx
@@ -4,8 +4,7 @@ import SvgGauge, { GaugeInstance, GaugeOptions } from "svg-gauge";
 import useProgressTodo from "@/hooks/todo/useProgressTodo";
 
 export default function Gauge() {
-  const { data } = useProgressTodo();
-  const progress = data?.progress ?? 0;
+  const { data, isLoading } = useProgressTodo();
   const gaugeEl = useRef<HTMLDivElement>(null);
   const gaugeRef = useRef<GaugeInstance>(null);
 
@@ -19,13 +18,15 @@ export default function Gauge() {
         label: (value) => Math.round(value) + "%",
       };
       gaugeRef.current = SvgGauge(gaugeEl.current, options);
-      gaugeRef.current?.setValue(1);
+      gaugeRef.current?.setValue(0);
     }
-    gaugeRef.current?.setValueAnimated(progress, 1);
-  }, [progress]);
+    gaugeRef.current?.setValueAnimated(data.progress, 2);
+  }, [data]);
+
+  if (isLoading) return null;
 
   return (
-    <div className="mt-[16px] flex flex-1 items-center">
+    <div className="mt-[16px] flex flex-1 items-center justify-center">
       <div className="h-[166px] w-[166px]">
         <div ref={gaugeEl}></div>
       </div>
